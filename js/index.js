66b32d467968b50f9dd37b43ad304282d1a4a0bf
Parima Shah <parima@openspace.ai>
2021-01-30 20:52:27 -0800

    Updated the icons

--- js/index.js
+++ js/index.js
@@ -27,34 +27,38 @@
         smooth: true
     });
 
+   imagesLoaded( document.getElementById('main'), () => {
+       scroll.update();
+   })
+
     
-    scroll.on('call', callValue => {
-       console.log(callValue);
-       if(callValue === 'bg-blue'){
-            const videoOverlay = document.getElementById('video-overlay');
-            const video = document.getElementById("video");
-            if(videoOverlay.classList.contains('bg-blue')){
-                videoOverlay.classList.remove('bg-blue');
-                video.style.opacity = 1;
-            }
-            else{
-                videoOverlay.classList.add('bg-blue');
-                setTimeout( () => {
-                    video.style.opacity = 0;
-                }, 1000);
-            }
-        }
-        if(callValue === 'bg-gray'){
-            const planeScroll = document.getElementById('plane-scroll');
-            if(planeScroll.classList.contains(callValue)){
-                planeScroll.classList.remove(callValue);
-            }
-            else{
-                planeScroll.classList.add(callValue);
-            }
-        }
+    // scroll.on('call', callValue => {
+    //    console.log(callValue);
+    //    if(callValue === 'bg-blue'){
+    //         const videoOverlay = document.getElementById('video-overlay');
+    //         const video = document.getElementById("video");
+    //         if(videoOverlay.classList.contains('bg-blue')){
+    //             videoOverlay.classList.remove('bg-blue');
+    //             video.style.opacity = 1;
+    //         }
+    //         else{
+    //             videoOverlay.classList.add('bg-blue');
+    //             setTimeout( () => {
+    //                 video.style.opacity = 0;
+    //             }, 1000);
+    //         }
+    //     }
+    //     if(callValue === 'bg-gray'){
+    //         const planeScroll = document.getElementById('plane-scroll');
+    //         if(planeScroll.classList.contains(callValue)){
+    //             planeScroll.classList.remove(callValue);
+    //         }
+    //         else{
+    //             planeScroll.classList.add(callValue);
+    //         }
+    //     }
 
 
-    });
+    // });
 
 })();
